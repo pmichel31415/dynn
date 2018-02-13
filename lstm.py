a58824b3b338e085463823792f640f30d8834aac
Paul Michel <pmichel31415@gmail.com>
2018-02-13 11:45:04 -0500

    better transduce

--- lstm.py
+++ lstm.py
@@ -8,11 +8,12 @@ import layers
 
 class NaryTreeLSTMCell(layers.Layer):
     """N-ary TreeLSTM as introduced in Tai et al, 2015"""
+
     def __init__(self, N, di, dh, pc, dropout=0.0, diagonal=False):
         super(NaryTreeLSTMCell, self).__init__(pc, '%d-treelstm' % N)
         # Arity
         self.N = N
-        assert self.N>=0, 'Arity %d not supported' % self.N
+        assert self.N >= 0, 'Arity %d not supported' % self.N
         # Output dimension
         self.dh = dh
         # Input dimension
@@ -35,7 +36,6 @@ class NaryTreeLSTMCell(layers.Layer):
         self.bg_p = self.pc.add_parameters(self.dh, name='bg', init=layers.ZeroInit)
         self.bf_p = [self.pc.add_parameters(self.dh, name='bf%d' % i, init=layers.OneInit) for i in range(self.N)]
 
-
     def init(self, test=False, update=True):
         # Load weights in computation graph
         self.Wi = self.Wi_p.expr(update)
@@ -48,13 +48,13 @@ class NaryTreeLSTMCell(layers.Layer):
         self.bg = self.bg_p.expr(update)
         self.bf = [w.expr(update) for w in self.bf_p]
         # Initialize dropout mask
-        self.test=test
-        if not test and self.dropout>0:
+        self.test = test
+        if not test and self.dropout > 0:
             self.dropout_mask = dy.dropout(dy.ones(self.incoming_dim), self.dropout)
 
     def __call__(self, *args):
         """Arguments are of the form h1, c1, h2, c2,...,h_N, c_N[, x]"""
-        if not (len(args) == (2 * self.N + 1) or (len(args) == (2 * self.N) and self.di==0)):
+        if not (len(args) == (2 * self.N + 1) or (len(args) == (2 * self.N) and self.di == 0)):
             error_msg = '%d arguments for %d-ary treeLSTM (should be 2 * arity + 1)' % (len(args), self.N)
             raise ValueError(error_msg)
         # Concatenate inputs
@@ -62,7 +62,7 @@ class NaryTreeLSTMCell(layers.Layer):
         c_m1 = args[1::2]
         h_m1 = dy.concatenate(list(h_m1))
         # Dropout
-        if not self.test and self.dropout>0:
+        if not self.test and self.dropout > 0:
             h = dy.cmult(self.dropout_mask, h_m1)
         # Compute gates
         self.i = dy.logistic(dy.affine_transform([self.bi, self.Wi, h_m1]))
@@ -75,20 +75,23 @@ class NaryTreeLSTMCell(layers.Layer):
             self.f = [dy.logistic(dy.affine_transform([self.bf[i], self.Wf[i], h_m1])) for i in range(self.N)]
         # Update c with gating
         self.c = dy.cmult(self.i, self.g)
-        if self.N>0:
+        if self.N > 0:
             self.c += dy.esum([dy.cmult(self.f[i], c_m1[i]) for i in range(self.N)])
         # Output
         self.h = dy.cmult(self.o, dy.tanh(self.c))
         # Return h and c
         return self.h, self.c
 
+
 def LSTM(di, dh, pc, dropout=0.0):
     """Standard LSTM"""
     return NaryTreeLSTMCell(1, di, dh, pc, dropout=dropout, diagonal=False)
 
+
 def BinaryTreeLSTM(di, dh, pc, dropout=0.0, diagonal=False):
     """Binary tree-lstm"""
     return NaryTreeLSTMCell(2, di, dh, pc, dropout=dropout, diagonal=diagonal)
+
 
 class CompactLSTM(layers.Layer):
     """Standard LSTM using dynet's memory efficient LSTM operations"""
@@ -100,7 +103,7 @@ class CompactLSTM(layers.Layer):
         self.dropout = dropout
 
         # Parameters
-        scale = np.sqrt(6/(2*self.dh + self.di))
+        scale = np.sqrt(6 / (2 * self.dh + self.di))
         self.Whx_p = self.pc.add_parameters((self.dh * 4, self.di), name='Whx', init=dy.UniformInitializer(scale))
         self.Whh_p = self.pc.add_parameters((self.dh * 4, self.dh), name='Whh', init=dy.UniformInitializer(scale))
         self.bh_p = self.pc.add_parameters((self.dh * 4,), name='bh', init=layers.ZeroInit)
@@ -111,33 +114,44 @@ class CompactLSTM(layers.Layer):
         self.Whh = self.Whh_p.expr(update)
         self.bh = self.bh_p.expr(update)
         # Initialize dropout mask
-        self.test=test
-        if not test and self.dropout>0:
+        self.test = test
+        if not test and self.dropout > 0:
             self.dropout_mask_x = dy.dropout(dy.ones(self.di), self.dropout)
             self.dropout_mask_h = dy.dropout(dy.ones(self.dh), self.dropout)
 
     def __call__(self, h, c, x):
-        if not self.test and self.dropout>0:
-            gates = dy.vanilla_lstm_gates_dropout(x,h,self.Whx, self.Whh,self.bh, self.dropout_mask_x, self.dropout_mask_h)
+        if not self.test and self.dropout > 0:
+            gates = dy.vanilla_lstm_gates_dropout(x, h, self.Whx, self.Whh, self.bh, self.dropout_mask_x,
+                                                  self.dropout_mask_h)
         else:
-            gates = dy.vanilla_lstm_gates(x,h,self.Whx, self.Whh,self.bh)
+            gates = dy.vanilla_lstm_gates(x, h, self.Whx, self.Whh, self.bh)
         new_c = dy.vanilla_lstm_c(c, gates)
         new_h = dy.vanilla_lstm_h(new_c, gates)
         return new_h, new_c
 
 
-def transduce(lstm, xs, h0, c0, masks=None):
+def transduce(lstm, xs, h0, c0, lengths=None, backward=False):
     """Helper function for LSTM transduction with masking"""
     h, c = h0, c0
     hs = []
-    if masks is not None:
-        for x, m in zip(xs, masks):
+    batch_size = xs[0].dim()[-1]
+    if backward:
+        xs = xs[::-1]
+    if lengths is not None:
+        for i, x in enumerate(xs):
             h, c = lstm(h, c, x)
             hs.append(h)
+            # Compute mask
+            if backward:
+                m = (np.full(batch_size, i) >= (len(xs) - lengths)).astype(int)
+            else:
+                m = (np.full(batch_size, i) < lengths).astype(int)
             m_e = dy.inputTensor(m, batched=True)
             h, c = dy.cmult(h, m_e), dy.cmult(c, m_e)
     else:
         for x in xs:
             h, c = lstm(h, c, x)
             hs.append(h)
+    if backward:
+        hs = hs[::-1]
     return hs
