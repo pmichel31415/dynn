7afaac83e8d061f138aaf0d7a6eaa90436a2d3d7
Paul Michel <pmichel31415@gmail.com>
2018-09-21 17:03:14 -0400

    residual_ -> shortcut_

--- dynn/layers/residual_layers.py
+++ dynn/layers/residual_layers.py
@@ -16,17 +16,17 @@ class ResidualLayer(BaseLayer):
     def __init__(
         self,
         layer,
-        residual_transform=None,
+        shortcut_transform=None,
         layer_weight=1.0,
-        residual_weight=1.0,
+        shortcut_weight=1.0,
     ):
         super(ResidualLayer, self).__init__("residual")
         self.layer = layer
-        self.residual_transform = _default_value(
-            residual_transform, IdentityLayer()
+        self.shortcut_transform = _default_value(
+            shortcut_transform, IdentityLayer()
         )
         self.layer_weight = layer_weight
-        self.residual_weight = residual_weight
+        self.shortcut_weight = shortcut_weight
 
     def init(self, *args, **kwargs):
         """Initialize the layer before performing computation
@@ -34,11 +34,11 @@ class ResidualLayer(BaseLayer):
         For example setup dropout, freeze some parameters, etc...
         """
         self.layer.init(*args, **kwargs)
-        self.residual_transform.init(*args, **kwargs)
+        self.shortcut_transform.init(*args, **kwargs)
 
     def __call__(self, *args, **kwargs):
         """Execute forward pass"""
         output = self.layer_weight * self.layer(*args, **kwargs)
-        residual_connections = self.residual_weight * \
-            self.residual_transform(*args, **kwargs)
-        return output + residual_connections
+        shortcut_connections = self.shortcut_weight * \
+            self.shortcut_transform(*args, **kwargs)
+        return output + shortcut_connections
--- tests/layers/test_residual_layers.py
+++ tests/layers/test_residual_layers.py
@@ -15,11 +15,11 @@ class TestResidualLayer(TestCase):
 
     def setUp(self):
         self.layer = functional_layers.LambdaLayer(activations.tanh)
-        self.residual_transformation = functional_layers.LambdaLayer(
+        self.shortcut_transformation = functional_layers.LambdaLayer(
             activations.sigmoid
         )
         self.layer_weight = 0.444
-        self.residual_weight = 0.222
+        self.shortcut_weight = 0.222
         self.inputs = [np.random.rand(2, 3) for _ in range(10)]
 
     def test_simple(self):
@@ -38,13 +38,13 @@ class TestResidualLayer(TestCase):
                 residual(x).npvalue(), (activations.tanh(x)+x).npvalue()
             ))
 
-    def test_residual_transformation(self):
+    def test_shortcut_transformation(self):
         # Create residual layer
         residual = residual_layers.ResidualLayer(
             self.layer,
-            residual_transform=self.residual_transformation,
+            shortcut_transform=self.shortcut_transformation,
             layer_weight=self.layer_weight,
-            residual_weight=self.residual_weight,
+            shortcut_weight=self.shortcut_weight,
         )
         # Iterate over different inputs
         for x_val in self.inputs:
@@ -58,7 +58,7 @@ class TestResidualLayer(TestCase):
             y = residual(x)
             # Expected output
             z = self.layer_weight * activations.tanh(x) + \
-                self.residual_weight * activations.sigmoid(x)
+                self.shortcut_weight * activations.sigmoid(x)
             # Check value
             self.assertTrue(np.allclose(y.npvalue(), z.npvalue()))
 
