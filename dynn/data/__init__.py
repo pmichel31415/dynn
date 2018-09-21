84b9e5aaf39b5a2ed8ebdffffb5b32ac26123230
Paul Michel <pmichel31415@gmail.com>
2018-09-21 17:40:25 -0400

    Add CIFAR-10 to supported datasets

--- dynn/data/__init__.py
+++ dynn/data/__init__.py
@@ -8,6 +8,7 @@ code for minibatching as well as functions for downloading common datasets.
 Supported datasets are:
 
 - `MNIST <http://yann.lecun.com/exdb/mnist/>`_
+- `CIFAR-10 <https://www.cs.toronto.edu/~kriz/cifar.html>`_
 """
 from . import (
     batching,
