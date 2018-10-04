bdf67b960c80f66a3d9ef9145f1dece8a75c7673
Paul Michel <pmichel31415@gmail.com>
2018-10-04 14:22:01 -0400

    One function to reset them all (the random seeds)

--- dynn/__init__.py
+++ dynn/__init__.py
@@ -2,6 +2,9 @@
 DyNN
 ====
 """
+import numpy as np
+import dynet as dy
+
 from . import layers
 from . import data
 from . import operations
@@ -17,3 +20,21 @@ __all__ = [
     "activations",
     "parameter_initialization"
 ]
+
+
+def set_random_seed(seed, numpy_seed=None):
+    """Set the random seeds.
+
+    This will fix the random seeds both for dynet (influences parameter
+    initialization, stochastic operations etc...) and numpy (used for data
+    shuffling mostly).
+
+    Args:
+        seed (int): Random seed
+        numpy_seed (int, optional): Optionally you can specify a different seed
+            for numpy (by default it will seed dynet with ``seed`` and numpy
+            with ``seed+1``)
+    """
+    numpy_seed = numpy_seed or (seed + 1)
+    dy.reset_random_seed(seed)
+    np.random.seed(numpy_seed)
