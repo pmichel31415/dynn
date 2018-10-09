3fe05ebc5e55ab86c91e202882fb35bea03bb77b
Paul Michel <pmichel31415@gmail.com>
2018-10-09 15:44:07 -0400

    merge

--- tests/layers/test_recurrent_layers.py
+++ tests/layers/test_recurrent_layers.py
@@ -7,19 +7,19 @@ import dynet as dy
 from dynn.layers import recurrent_layers
 
 
-def _test_recurrent_layer(layer, dummy_input):
+def _test_recurrent_layer(cell, dummy_input):
     # Initialize computation graph
     dy.renew_cg()
     # Create inputs
     x = dy.inputTensor(dummy_input)
-    # Initialize layer
-    layer.init(test=False, update=True)
-    # Run layer
-    state = layer.initial_value()
-    state = layer(x, *state)
-    state = layer(x + 1, *state)
+    # Initialize cell
+    cell.init(test=False, update=True)
+    # Run cell
+    state = cell.initial_value()
+    state = cell(x, *state)
+    state = cell(x + 1, *state)
     # Try forward/backward
-    z = dy.sum_elems(state[0])
+    z = dy.sum_elems(cell.get_output(state))
     z.forward()
     z.backward()
 
@@ -58,6 +58,57 @@ class TestLSTM(TestCase):
             dropout_h=self.dropout,
         )
         _test_recurrent_layer(lstm, np.random.rand(self.di))
+
+
+class TestStackedRecurrentCells(TestCase):
+
+    def setUp(self):
+        self.pc = dy.ParameterCollection()
+        self.dims = [20, 10, 15]
+        self.dropout = 0.1
+
+    def test_lstm_lstm(self):
+        sub_cells = [
+            recurrent_layers.LSTM(
+                self.pc,
+                self.dims[0],
+                self.dims[1],
+                dropout_x=self.dropout,
+                dropout_h=self.dropout,
+            ),
+            recurrent_layers.LSTM(
+                self.pc,
+                self.dims[1],
+                self.dims[2],
+                dropout_x=self.dropout,
+                dropout_h=self.dropout,
+            )
+        ]
+        # Create the stacked layer
+        cell = recurrent_layers.StackedRecurrentCells(*sub_cells)
+        # Test
+        _test_recurrent_layer(cell, np.random.rand(self.dims[0]))
+
+    def test_lstm_rnn(self):
+        sub_cells = [
+            recurrent_layers.LSTM(
+                self.pc,
+                self.dims[0],
+                self.dims[1],
+                dropout_x=self.dropout,
+                dropout_h=self.dropout,
+            ),
+            recurrent_layers.ElmanRNN(
+                self.pc,
+                self.dims[1],
+                self.dims[2],
+                dropout=self.dropout
+            )
+        ]
+        # Create the stacked layer
+        cell = recurrent_layers.StackedRecurrentCells(*sub_cells)
+        # Test
+        _test_recurrent_layer(cell, np.random.rand(self.dims[0]))
 
 
 if __name__ == '__main__':
