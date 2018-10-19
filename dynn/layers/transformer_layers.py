6ff299ee4ef56570df682169306b7fa059d6c732
Paul Michel <pmichel31415@gmail.com>
2018-10-19 11:51:00 -0400

    Conditional transformer

--- dynn/layers/transformer_layers.py
+++ dynn/layers/transformer_layers.py
@@ -4,6 +4,7 @@ Transformer layers
 ==================
 """
 import numpy as np
+import dynet as dy
 
 from ..operations import unsqueeze, seq_mask
 from ..activations import relu
@@ -15,7 +16,39 @@ from .dense_layers import Affine
 from .combination_layers import Sequential
 
 
-class TransformerLayer(ParametrizedLayer):
+def _transformer_mask(x, triu, mask, lengths, left_aligned):
+    """Helper function to determine attention masks for the transformer
+
+    Args:
+        x (:py:class:`dynet.Expression`): Input (dimensions
+            ``hidden_dim x L``)
+        triu (bool): Upper triangular masking
+        mask (:py:class:`dynet.Expression`): Mask expression
+            (if ``triu`` is ``False``)
+        lengths (list, optional): List of lengths (if ``mask`` is ``None``)
+        left_aligned (bool, optional): Alignment (if ``mask`` is ``None``)
+
+    Returns:
+        :py:class:`dynet.Expression`, None: The actual mask
+    """
+    if triu:
+        # Upper triangular mask
+        _, L = x.dim()[0]
+        # We want 0s in the upper triangualr part, so -inf in the lower
+        # triangular part -> np.tril
+        mask = dy.inputTensor(np.tril(np.full((L, L), -np.inf), -1))
+    elif mask is not None:
+        # Precomputed mask
+        if len(mask.dim()[0]) == 1:
+            mask = unsqueeze(mask, d=-1)
+    elif lengths is not None and mask is None:
+        # Recompite mask given lengths and alignment
+        _, L = x.dim()[0]
+        mask = seq_mask(L, lengths, 0, -np.inf, left_aligned)
+    return mask
+
+
+class Transformer(ParametrizedLayer):
     """Transformer layer.
 
     As described in `Vaswani et al. (2017) <https://arxiv.org/abs/1706.03762>`_
@@ -38,7 +71,7 @@ class TransformerLayer(ParametrizedLayer):
         activation=relu,
         dropout=0.0,
     ):
-        super(TransformerLayer, self).__init__(pc, "mlp-attention")
+        super(Transformer, self).__init__(pc, "transformer")
         # Hyper-parameters
         self.n_heads = n_heads
         self.hidden_dim = hidden_dim
@@ -54,7 +87,7 @@ class TransformerLayer(ParametrizedLayer):
             Affine(self.pc, d, d, activation=activation),
             Affine(self.pc, d, d),
         )
-        self.layer_norm_mlp = LayerNorm(self.pc, (1, d))
+        self.layer_norm_mlp = LayerNorm(self.pc, (d, 1))
 
     def init(self, test=True, update=False):
         self.self_att.init(test=test, update=update)
@@ -68,6 +101,7 @@ class TransformerLayer(ParametrizedLayer):
         x,
         lengths=None,
         left_aligned=True,
+        triu=False,
         mask=None,
         return_att=False
     ):
@@ -77,10 +111,13 @@ class TransformerLayer(ParametrizedLayer):
         length dimension.
 
         Args:
-            x (:py:class:`dynet.Expression`): Input (dimensions ``d x L``)
+            x (:py:class:`dynet.Expression`): Input (dimensions
+                ``hidden_dim x L``)
             lengths (list, optional): Defaults to None. List of lengths for
                 masking (used for attention)
-            left_aligned (bool, optional): Defaults to True. USed for masking
+            left_aligned (bool, optional): Defaults to True. Used for masking
+            triu (bool, optional): Upper triangular self attention. Mask such
+                that each position can only attend to the previous positions.
             mask (:py:class:`dynet.Expression`, optional): Defaults to None.
                 As an alternative to ``length``, you can pass a mask
                 expression directly (useful to reuse masks accross layers)
@@ -95,12 +132,7 @@ class TransformerLayer(ParametrizedLayer):
         if len(x.dim()[0]) == 1:
             x = unsqueeze(x, d=-1)
         # Masking
-        if mask is not None:
-            if len(mask.dim()[0]) == 1:
-                mask = unsqueeze(mask, d=-1)
-        elif lengths is not None and mask is None:
-            _, L = x.dim()[0]
-            mask = seq_mask(L, lengths, 0, -np.inf, left_aligned)
+        mask = _transformer_mask(x, triu, mask, lengths, left_aligned)
         # Self attend
         h_att, weights = self.self_att(x, x, x, mask)
         # Dropout + residual + normalization
@@ -110,7 +142,7 @@ class TransformerLayer(ParametrizedLayer):
         h_mlp = self.mlp(h_att + x)
         # Residual + normalization
         h_att_dropped = conditional_dropout(h_att, self.dropout, not self.test)
-        h_mlp = self.layer_norm_att(h_mlp + h_att_dropped, d=1)
+        h_mlp = self.layer_norm_mlp(h_mlp + h_att_dropped, d=1)
         # Return
         if return_att:
             return h_mlp, weights
@@ -118,7 +150,7 @@ class TransformerLayer(ParametrizedLayer):
             return h_mlp
 
 
-class StackedTransformerLayers(Sequential):
+class StackedTransformers(Sequential):
     """Multilayer transformer.
 
     Args:
@@ -142,17 +174,18 @@ class StackedTransformerLayers(Sequential):
     ):
         # Instatiate layers
         tf_layers = [
-            TransformerLayer(pc, hidden_dim, n_heads, activation, dropout)
+            Transformer(pc, hidden_dim, n_heads, activation, dropout)
             for _ in range(n_layers)
         ]
         # Initialize
-        super(StackedTransformerLayers, self).__init__(*tf_layers)
+        super(StackedTransformers, self).__init__(*tf_layers)
 
     def __call__(
         self,
         x,
         lengths=None,
         left_aligned=True,
+        triu=False,
         mask=None,
         return_att=False,
         return_last_only=True,
@@ -163,10 +196,13 @@ class StackedTransformerLayers(Sequential):
         length dimension.
 
         Args:
-            x (:py:class:`dynet.Expression`): Input (dimensions ``d x L``)
+            x (:py:class:`dynet.Expression`): Input (dimensions
+                ``hidden_dim x L``)
             lengths (list, optional): Defaults to None. List of lengths for
                 masking (used for attention)
             left_aligned (bool, optional): Defaults to True. USed for masking
+            triu (bool, optional): Upper triangular self attention. Mask such
+                that each position can only attend to the previous positions.
             mask (:py:class:`dynet.Expression`, optional): Defaults to None.
                 As an alternative to ``length``, you can pass a mask
                 expression directly (useful to reuse masks accross layers)
@@ -183,12 +219,7 @@ class StackedTransformerLayers(Sequential):
         if len(x.dim()[0]) == 1:
             x = unsqueeze(x, d=-1)
         # Masking
-        if mask is not None:
-            if len(mask.dim()[0]) == 1:
-                mask = unsqueeze(mask, d=-1)
-        elif lengths is not None and mask is None:
-            _, L = x.dim()[0]
-            mask = seq_mask(L, lengths, 0, -np.inf, left_aligned)
+        mask = _transformer_mask(x, triu, mask, lengths, left_aligned)
         # Keep track each layer's output
         outputs = ([x], [None])
         # Run all layers
@@ -196,6 +227,269 @@ class StackedTransformerLayers(Sequential):
             new_h, weights = layer(outputs[0][-1], mask=mask, return_att=True)
             outputs[0].append(new_h)
             outputs[1].append(weights)
+        # Discard weights if they're not needed
+        if not return_att:
+            outputs = outputs[0]
+        # Return
+        if return_last_only:
+            return outputs[-1]
+        else:
+            return outputs[1:]
+
+
+class CondTransformer(ParametrizedLayer):
+    """Conditional transformer layer.
+
+    As described in `Vaswani et al. (2017) <https://arxiv.org/abs/1706.03762>`_
+    This is the "decoder" side of the transformer, ie self attention +
+    attention to context.
+
+    Args:
+        pc (:py:class:`dynet.ParameterCollection`): Parameter collection to
+            hold the parameters
+        hidden_dim (int): Hidden dimension (used everywhere)
+        cond_dim (int): Conditional dimension (dimension of the "encoder" side,
+            used for attention)
+        n_heads (int): Number of heads for attention.
+        activation (function, optional): MLP activation (defaults to relu).
+        dropout (float, optional): Dropout rate (defaults to 0)
+    """
+
+    def __init__(
+        self,
+        pc,
+        hidden_dim,
+        cond_dim,
+        n_heads,
+        activation=relu,
+        dropout=0.0,
+    ):
+        super(CondTransformer, self).__init__(pc, "cond-transformer")
+        # Hyper-parameters
+        self.n_heads = n_heads
+        self.hidden_dim = hidden_dim
+        self.cond_dim = cond_dim
+        self.activation = activation
+        self.dropout = dropout
+        # Sub layers
+        d = hidden_dim
+        dc = cond_dim
+        # Self-attention
+        self.self_att = MultiHeadAttention(self.pc, n_heads, d, d, d, d, d)
+        self.layer_norm_self_att = LayerNorm(self.pc, (d, 1))
+        # Conditional attention
+        self.cond_att = MultiHeadAttention(self.pc, n_heads, d, dc, dc, d, d)
+        self.layer_norm_cond_att = LayerNorm(self.pc, (d, 1))
+        # MLP
+        self.mlp = Sequential(
+            Affine(self.pc, d, d, activation=activation),
+            Affine(self.pc, d, d),
+        )
+        self.layer_norm_mlp = LayerNorm(self.pc, (d, 1))
+
+    def init(self, test=True, update=False):
+        self.self_att.init(test=test, update=update)
+        self.layer_norm_self_att.init(test=test, update=update)
+        self.cond_att.init(test=test, update=update)
+        self.layer_norm_cond_att.init(test=test, update=update)
+        self.mlp.init(test=test, update=update)
+        self.layer_norm_mlp.init(test=test, update=update)
+        self.test = test
+
+    def __call__(
+        self,
+        x,
+        c,
+        lengths=None,
+        left_aligned=True,
+        mask=None,
+        triu=False,
+        lengths_c=None,
+        left_aligned_c=True,
+        mask_c=None,
+        return_att=False,
+    ):
+        """Run the transformer layer.
+
+        The input is expected to have dimensions ``d x L`` where ``L`` is the
+        length dimension.
+
+        Args:
+            x (:py:class:`dynet.Expression`): Input (dimensions
+                ``hidden_dim x L``)
+            c (:py:class:`dynet.Expression`): Context (dimensions
+                ``cond_dim x L``)
+            lengths (list, optional): Defaults to None. List of lengths for
+                masking (used for self attention)
+            left_aligned (bool, optional): Defaults to True. USed for masking
+                in self attention.
+            mask (:py:class:`dynet.Expression`, optional): Defaults to None.
+                As an alternative to ``length``, you can pass a mask
+                expression directly (useful to reuse masks accross layers).
+            triu (bool, optional): Upper triangular self attention. Mask such
+                that each position can only attend to the previous positions.
+            lengths_c (list, optional): Defaults to None. List of lengths for
+                masking (used for conditional attention)
+            left_aligned_c (bool, optional): Defaults to True. Used for masking
+                in conditional attention.
+            mask_c (:py:class:`dynet.Expression`, optional): Defaults to None.
+                As an alternative to ``length_c``, you can pass a mask
+                expression directly (useful to reuse masks accross layers).
+            return_att (bool, optional): Defaults to False. Return the self and
+                conditional attention weights
+
+        Returns:
+            tuple, :py:class:`dynet.Expression`: The output expression (+ the
+                attention weights if ``return_att`` is ``True``)
+        """
+        # Input has shape (d, L), B
+        if len(x.dim()[0]) == 1:
+            x = unsqueeze(x, d=-1)
+        # Context has shape (dc, l), B
+        if len(c.dim()[0]) == 1:
+            c = unsqueeze(c, d=-1)
+        # Masking
+        mask = _transformer_mask(x, triu, mask, lengths, left_aligned)
+        # Masking (conditional attention)
+        mask_c = _transformer_mask(c, False, mask_c, lengths_c, left_aligned_c)
+        # Self attend
+        h_att, self_weights = self.self_att(x, x, x, mask)
+        # Dropout + residual + normalization
+        x_drop = conditional_dropout(x, self.dropout, not self.test)
+        h_att = self.layer_norm_self_att(h_att + x_drop, d=1)
+        # Conditional attention
+        h_cond, cond_weights = self.cond_att(h_att, c, c, mask_c)
+        # Dropout + residual + normalization
+        h_att_drop = conditional_dropout(h_att, self.dropout, not self.test)
+        h_cond = self.layer_norm_cond_att(h_cond + h_att_drop, d=1)
+        # MLP
+        h_mlp = self.mlp(h_att + x)
+        # Residual + normalization
+        h_cond_drop = conditional_dropout(h_cond, self.dropout, not self.test)
+        h_mlp = self.layer_norm_mlp(h_mlp + h_cond_drop, d=1)
+        # Return
+        if return_att:
+            return h_mlp, self_weights, cond_weights
+        else:
+            return h_mlp
+
+
+class StackedCondTransformers(Sequential):
+    """Multilayer transformer.
+
+    Args:
+        pc (:py:class:`dynet.ParameterCollection`): Parameter collection to
+            hold the parameters
+        n_layers (int): Number of layers
+        hidden_dim (int): Hidden dimension (used everywhere)
+        cond_dim (int): Conditional dimension (dimension of the "encoder" side,
+            used for attention)
+        n_heads (int): Number of heads for self attention.
+        activation (function, optional): MLP activation (defaults to relu).
+        dropout (float, optional): Dropout rate (defaults to 0)
+    """
+
+    def __init__(
+        self,
+        pc,
+        n_layers,
+        hidden_dim,
+        cond_dim,
+        n_heads,
+        activation=relu,
+        dropout=0.0
+    ):
+        # Instatiate layers
+        tf_layers = [
+            CondTransformer(pc, hidden_dim, cond_dim,
+                            n_heads, activation, dropout)
+            for _ in range(n_layers)
+        ]
+        # Initialize
+        super(StackedCondTransformers, self).__init__(*tf_layers)
+
+    def __call__(
+        self,
+        x,
+        c,
+        lengths=None,
+        left_aligned=True,
+        mask=None,
+        triu=False,
+        lengths_c=None,
+        left_aligned_c=True,
+        mask_c=None,
+        return_att=False,
+        return_last_only=True,
+    ):
+        """Run the multilayer transformer.
+
+        The input is expected to have dimensions ``d x L`` where ``L`` is the
+        length dimension.
+
+        Args:
+            x (:py:class:`dynet.Expression`): Input (dimensions
+                ``hidden_dim x L``)
+            c (list): list of contexts (one per layer, each of dim
+                ``cond_dim x L``). If this is not a list (but an expression),
+                the same context will be used for each layer.
+            lengths (list, optional): Defaults to None. List of lengths for
+                masking (used for self attention)
+            left_aligned (bool, optional): Defaults to True. USed for masking
+                in self attention.
+            mask (:py:class:`dynet.Expression`, optional): Defaults to None.
+                As an alternative to ``length``, you can pass a mask
+                expression directly (useful to reuse masks accross layers).
+            triu (bool, optional): Upper triangular self attention. Mask such
+                that each position can only attend to the previous positions.
+            lengths_c (list, optional): Defaults to None. List of lengths for
+                masking (used for conditional attention)
+            left_aligned_c (bool, optional): Defaults to True. Used for masking
+                in conditional attention.
+            mask_c (:py:class:`dynet.Expression`, optional): Defaults to None.
+                As an alternative to ``length_c``, you can pass a mask
+                expression directly (useful to reuse masks accross layers).
+            return_last_only (bool, optional): Return only the output of the
+                last layer (as opposed to the output of all layers).
+
+        Returns:
+            tuple, :py:class:`dynet.Expression`: The output expression (+ the
+                attention weights if ``return_att`` is ``True``)
+        """
+        # Input has shape (d, L), B
+        if len(x.dim()[0]) == 1:
+            x = unsqueeze(x, d=-1)
+        # Context is a list
+        if not isinstance(c, list):
+            c = [c for _ in range(len(self.layers))]
+        elif len(c) == 1:
+            c = [c[0] for _ in range(len(self.layers))]
+        # Check context size
+        if len(c) != len(self.layers):
+            raise ValueError(
+                f"Must have either 1 or {len(self.layers)} in "
+                f"{len(self.layers)}-layered conditional transformer "
+                f"(got {len(c)})."
+            )
+        # Masking
+        mask = _transformer_mask(x, triu, mask, lengths, left_aligned)
+        mask_c = _transformer_mask(
+            c[0],
+            False,
+            mask_c,
+            lengths_c,
+            left_aligned_c
+        )
+        # Keep track each layer's output
+        outputs = ([x], [None], [None])
+        # Run all layers
+        for layer, c_ in zip(self.layers, c):
+            new_h, self_weights, cond_weights = layer(
+                outputs[0][-1], c_, mask=mask, mask_c=mask_c, return_att=True
+            )
+            outputs[0].append(new_h)
+            outputs[1].append(self_weights)
+            outputs[2].append(cond_weights)
         # Discard weights if they're not needed
         if not return_att:
             outputs = outputs[0]
--- tests/layers/test_transformer_layers.py
+++ tests/layers/test_transformer_layers.py
@@ -13,7 +13,7 @@ from dynn.layers import transformer_layers
 set_random_seed(14153)
 
 
-class TestTransformerLayer(TestCase):
+class TestTransformer(TestCase):
 
     def setUp(self):
         self.pc = dy.ParameterCollection()
@@ -59,7 +59,7 @@ class TestTransformerLayer(TestCase):
 
     def test_transformer_layer(self):
         # Create layer
-        transform = transformer_layers.TransformerLayer(
+        transform = transformer_layers.Transformer(
             self.pc,
             self.d,
             self.nh,
@@ -69,7 +69,7 @@ class TestTransformerLayer(TestCase):
 
     def test_stacked_transformer_layer(self):
         # Create layer
-        transform = transformer_layers.StackedTransformerLayers(
+        transform = transformer_layers.StackedTransformers(
             self.pc,
             self.nl,
             self.d,
@@ -77,6 +77,102 @@ class TestTransformerLayer(TestCase):
             dropout=self.dropout
         )
         self._test_transformer_layer(transform)
+
+    def test_triu_masking(self):
+        transform = transformer_layers.Transformer(
+            self.pc,
+            self.d,
+            self.nh,
+            dropout=self.dropout
+        )
+        for pos_loss in range(self.L):
+            dy.renew_cg()
+            x = dy.random_uniform((self.d, self.L), - 1,
+                                  1, batch_size=self.bsz)
+            # Initialize layer
+            transform.init(test=False, update=True)
+            # Run transformer
+            y = transform(x, triu=True)
+            # Sum of values at position pos_loss
+            z = dy.sum_batches(y[0][pos_loss])
+            # Forward backward
+            z.forward()
+            z.backward(full=True)
+            # Check gradients
+            gradients = x.gradient()
+            for pos_grad in range(self.L):
+                grad_at_pos = gradients[:, pos_grad, :]
+                is_masked = pos_grad > pos_loss
+                grad_is_zero = np.allclose(grad_at_pos, 0.0)
+                self.assertEqual(is_masked, grad_is_zero)
+
+
+class TestCondTransformer(TestCase):
+
+    def setUp(self):
+        self.pc = dy.ParameterCollection()
+        self.nl = 3
+        self.d = 8
+        self.dc = 2
+        self.nh = 4
+        self.l_ = 7
+        self.L = 6
+        self.bsz = self.l_
+        self.lengths = list(range(1, self.bsz + 1))
+        self.dropout = 0.01
+
+    def _test_cond_transformer(self, transform):
+        # Initialize computation graph
+        dy.renew_cg()
+        # Create inputs
+        x = dy.random_uniform((self.d, self.L), -1, 1, batch_size=self.bsz)
+        c = dy.random_uniform((self.dc, self.l_), -1, 1, batch_size=self.bsz)
+        # Initialize layer
+        transform.init(test=False, update=True)
+        # Run transformer
+        y = transform(x, c, lengths_c=self.lengths, triu=True)
+        # Average with masking
+        z = dy.sum_batches(dy.sum_elems(y[0]))
+        # Forward backward
+        z.forward()
+        z.backward(full=True)
+        # Check dimension
+        self.assertTupleEqual(y.dim()[0], (self.d, self.L))
+        self.assertEqual(y.dim()[1], self.bsz)
+        # Check masking
+        gradients = c.gradient()
+        print(gradients)
+        for b, length in enumerate(self.lengths):
+            grad_elem = gradients[:, :, b].T
+            for pos, g_val in enumerate(grad_elem):
+                is_masked = pos >= length
+                zero_grad = np.allclose(g_val, 0)
+                print(b, pos, is_masked)
+                self.assertEqual(is_masked, zero_grad)
+                self.assertTrue(not is_masked or zero_grad)
+
+    def test_cond_transformer(self):
+        # Create layer
+        transform = transformer_layers.CondTransformer(
+            self.pc,
+            self.d,
+            self.dc,
+            self.nh,
+            dropout=self.dropout
+        )
+        self._test_cond_transformer(transform)
+
+    def test_stacked_cond_transformer(self):
+        # Create layer
+        transform = transformer_layers.StackedCondTransformers(
+            self.pc,
+            self.nl,
+            self.d,
+            self.dc,
+            self.nh,
+            dropout=self.dropout
+        )
+        self._test_cond_transformer(transform)
 
 
 if __name__ == '__main__':
