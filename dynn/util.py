c329f2ff635543887add5465623f9dab74d75783
Paul Michel <pmichel31415@gmail.com>
2018-11-13 10:34:03 -0500

    Number of parameters

--- dynn/util.py
+++ dynn/util.py
@@ -185,3 +185,21 @@ def _should_mask(step, min_length, max_length, left_padded):
         return step >= min_length
     else:
         return step + min_length < max_length
+
+
+def num_params(pc):
+    """Number of parameters in a given ParameterCollection"""
+    N = 0
+    # All parameters
+    for p in pc.parameters_list():
+        p_size = 1
+        for d in p.shape():
+            p_size *= d
+        N += p_size
+    # All lookup parameters
+    for lp in pc.lookup_parameters_list():
+        lp_size = 1
+        for d in lp.shape():
+            lp_size *= d
+        N += lp_size
+    return N
